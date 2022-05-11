62aac4c4aeb8060b0845656eb1d5b233c1471a7c
Seohyun-Back <lucymail100@naver.com>
2022-05-12 02:00:36 +0900

    먼슬리에 정리 창까지 만듦

--- lib/screens/monthly.dart
+++ lib/screens/monthly.dart
@@ -13,83 +13,133 @@ class MonthlyWork extends StatefulWidget {
 }
 
 class _MonthlyWorkState extends State<MonthlyWork> {
-  DateTime _currentDate = DateTime.now();
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // appBar: PreferredSize(
-      //   preferredSize: Size.fromHeight(50.0), // AppBar 사이즈 지정
-      //   child: AppBar(
-      //     backgroundColor: Colors.white, // AppBar 색상 지정
-
-      //     leading: IconButton(
-      //       onPressed: () {
-      //         Navigator.pop(context);
-      //       },
-      //       icon: Icon(Icons.arrow_back_ios_rounded),
-      //     ),
-
-      //     iconTheme: IconThemeData(color: Color.fromARGB(255, 32, 32, 32)),
-      //     elevation: 0.0,
-
-      //     centerTitle: true,
-      //   ),
-      // ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 23.0),
-        child: CalendarCarousel<Event>(
-          onDayPressed: (DateTime date, List<Event> events) {
-            this.setState(() => _currentDate = date);
-          },
-          thisMonthDayBorderColor: Colors.transparent,
-//      weekDays: null, /// for pass null when you do not want to render weekDays
-//      headerText: Container( /// Example for rendering custom header
-//        child: Text('Custom Header'),
-//      ),
-          // customDayBuilder: (
-          //   /// you can provide your own build function to make custom day containers
-          //   bool isSelectable,
-          //   int index,
-          //   bool isSelectedDay,
-          //   bool isToday,
-          //   bool isPrevMonthDay,
-          //   TextStyle textStyle,
-          //   bool isNextMonthDay,
-          //   bool isThisMonthDay,
-          //   DateTime day,
-          // ) {
-          //   /// If you return null, [CalendarCarousel] will build container for current [day] with default function.
-          //   /// This way you can build custom containers for specific days only, leaving rest as default.
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  child: CalendarCarousel<Event>(
+                    onDayPressed: (DateTime date, List<Event> events) {
+                      this.setState(() => selectedDate = date);
+                    },
+                    thisMonthDayBorderColor: Colors.transparent,
+                    headerTextStyle: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'rouf-font',
+                      fontSize: 20.0,
+                    ),
+                    iconColor: Colors.black,
+                    weekdayTextStyle:
+                        TextStyle(fontSize: 14.0, color: Colors.grey),
+                    weekendTextStyle:
+                        TextStyle(fontSize: 15.0, color: Colors.black),
+                    todayTextStyle:
+                        TextStyle(fontSize: 15.0, color: Colors.black),
+                    todayBorderColor: Colors.transparent,
+                    todayButtonColor: Color.fromARGB(255, 245, 242, 242),
+                    weekFormat: false,
+                    height: 420.0,
+                    selectedDateTime: selectedDate,
+                    daysHaveCircularBorder: true,
 
-          //   // Example: every 15th of month, we have a flight, we can place an icon in the container like that:
-          //   if (day.day == 15) {
-          //     return Center(
-          //       child: Icon(Icons.local_airport),
-          //     );
-          //   } else {
-          //     return null;
-          //   }
-          // },
-          headerTextStyle: TextStyle(
-            color: Colors.black,
-            fontFamily: 'rouf-font',
-            fontSize: 20.0,
+                    /// null for not rendering any border, true for circular border, false for rectangular border
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 18, 20, 20),
+                height: 400,
+                width: 320,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text('listview 만들자'),
+                // child: Column(
+                //   children: [
+                //     StreamBuilder(
+                //       stream: FirebaseFirestore.instance
+                //           .collection('user/${globals.currentUid}/friends')
+                //           .snapshots(),
+                //       builder: (BuildContext context,
+                //           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>>
+                //               snapshot) {
+                //         if (snapshot.connectionState ==
+                //             ConnectionState.waiting) {
+                //           return Center(
+                //             child: CircularProgressIndicator(),
+                //           );
+                //         }
+                //         final docs = snapshot.data!.docs;
+
+                //         return Container(
+                //           height: 218,
+                //           child: ListView.builder(
+                //             scrollDirection: Axis.vertical,
+                //             shrinkWrap: true,
+                //             itemCount: docs.length,
+                //             itemBuilder: (context, index) {
+                //               return StreamBuilder(
+                //                   stream: FirebaseFirestore.instance
+                //                       .collection('user')
+                //                       .doc('${docs[index]['uid']}')
+                //                       .snapshots(),
+                //                   builder: (BuildContext context,
+                //                       AsyncSnapshot<
+                //                               DocumentSnapshot<
+                //                                   Map<String, dynamic>>>
+                //                           snapshot2) {
+                //                     if (snapshot.connectionState ==
+                //                         ConnectionState.waiting) {
+                //                       return Center(
+                //                         child: CircularProgressIndicator(),
+                //                       );
+                //                     }
+                //                     actionKey = snapshot2.data!['statusKey'];
+
+                //                     return Container(
+                //                         //padding:,
+                //                         child: ListTile(
+                //                       horizontalTitleGap: 6.0,
+                //                       dense: true,
+                //                       // 친구 프로필사진
+                //                       leading: CircleAvatar(
+                //                         radius: 18,
+                //                         backgroundColor: Color(0xff95DF7D),
+                //                         child: Image.asset(
+                //                           'images/TaskIcon/${globals.tasks[actionKey]}.png',
+                //                           height: 20,
+                //                           width: 20,
+                //                         ),
+                //                       ),
+                //                       // 친구 이름
+                //                       title: Text(
+                //                           docs[index]['name'] +
+                //                               '는 ' +
+                //                               globals.action[actionKey],
+                //                           style: TextStyle(fontSize: 18)),
+                //                     ));
+                //                   });
+                //             },
+                //           ),
+                //         );
+                //       },
+                //     )
+                //   ],
+                // )
+              ),
+            ],
           ),
-          iconColor: Colors.black,
-          weekdayTextStyle: TextStyle(fontSize: 14.0, color: Colors.grey),
-          weekendTextStyle: TextStyle(fontSize: 15.0, color: Colors.black),
-          todayTextStyle: TextStyle(fontSize: 15.0, color: Colors.black),
-          todayBorderColor: Colors.transparent,
-          todayButtonColor: Color.fromARGB(255, 245, 242, 242),
-          weekFormat: false,
-          // markedDatesMap: _markedDateMap,
-          height: 420.0,
-          selectedDateTime: _currentDate,
-          daysHaveCircularBorder: true,
-
-          /// null for not rendering any border, true for circular border, false for rectangular border
         ),
       ),
     );
